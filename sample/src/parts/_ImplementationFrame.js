65d24c81a7085da4ae572444f4d89636bc302ce4
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-12 22:25:48 +0200

    updated UI

--- sample/src/parts/_ImplementationFrame.js
+++ sample/src/parts/_ImplementationFrame.js
@@ -1,34 +1,17 @@
 /* eslint-disable no-unused-vars */
-import { Box, Tooltip } from '@material-ui/core'
-import { green } from '@material-ui/core/colors'
-import { AmpStories } from '@material-ui/icons'
-import AddToHomeScreenIcon from '@material-ui/icons/AddToHomeScreen'
-import AppsIcon from '@material-ui/icons/Apps'
-import AspectRatioIcon from '@material-ui/icons/AspectRatio'
-import CastConnectedIcon from '@material-ui/icons/CastConnected'
-import HomeIcon from '@material-ui/icons/Home'
-import Skeleton from '@material-ui/lab/Skeleton'
+import { Tooltip } from '@material-ui/core'
 import { useState } from 'react'
 import { MuiPanelProvider } from '../components/MuiPanelStore'
 import MuiStatusBar from '../components/MuiStatusBar'
-import MupButton from '../components/MupButton'
 import MupContent from '../components/MupContent'
-import MupPanel from '../components/MupPanel'
-import MupStatus from '../components/MupStatus'
-import ActButton from './ActButton'
 import AliveStatus from './AliveStatus'
-import AnotherIframePanel from './AnotherIframePanel'
 import AvatarsStatuses from './AvatarsStatuses'
 import AvatarStatus from './AvatarStatus'
 import CastStatus from './CastStatus'
-import ComplexPanel from './ComplexPanel'
 import ConfigStatus from './ConfigStatus'
 import DynamicInjectStatus from './DynamicInjectStatus'
-import DynamicStatus from './DynamicStatus'
 import GalleryStatus from './GalleryStatus'
-import IframePanel from './IframePanel'
 import MenuStatus from './MenuStatus'
-import NotificationPanel from './NotificationPanel'
 import SaveStatus from './SaveStatus'
 import ToggleTheme from './ToggleTheme'
 
@@ -41,11 +24,7 @@ function ImplementationFrame({
   inverseMarkers, toggleInverseMarkers,
   wikiUrl, setWikiUrl
 }) {
-
   const [markerColor, setMarkerColor] = useState('primary')
-  const refreshPage = () => window.location.reload(false)
-
-  const toggleFullScreen = () => { }
 
   const statusBlock = <>
     <AvatarStatus />
@@ -77,74 +56,8 @@ function ImplementationFrame({
       markerColor, setMarkerColor
     }} />
 
-    {/* buttons section  */}
-    {/* <MupButton
-          id="logoAndText"
-          tooltip="Custom Tooltip &amp; Color"
-          shortText="HOME"
-          icon={<HomeIcon style={{ color: green[500] }} />}
-        />
 
-        <MupButton
-          id="logoDisabled"
-          disabled
-          icon={<AppsIcon style={{ color: green[500] }} />}
-        />
-
-        <MupButton
-          id="logoColored"
-          onClick={() => setWikiUrl("https://www.wikidata.org/wiki/Wikidata:Main_Page")}
-          tooltip="Custom Tooltip &amp; and default MUI Color"
-          icon={<AppsIcon color="primary" />}
-        />
-
-        <ActButton /> */}
-
-    {/* status section  */}
     {statusBlock}
-    {/* <MupStatus tooltip="Image with no text" id='fooImageTexrStatus' elements={[{ image: 'https://picsum.photos/32/32', mask: true, text: "User" }]}/> */}
-
-    {/* panel section  */}
-    {/* <NotificationPanel />
-        <NotificationPanel icon={<AddToHomeScreenIcon />} identifier="notificationsPanel2" title="Other Panel Triggering notifications" />
-        <ComplexPanel />
-        <IframePanel/>
-        <AnotherIframePanel /> */}
-
-    <ComplexPanel />
-    <IframePanel />
-    <AnotherIframePanel />
-
-    {/* <MupPanel placement="bottom" id="randomText44" title="Lorem Ipsum Panel" icon={<FormatIndentIncrease />}>
-          <Typography color="textPrimary">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</Typography>
-        </MupPanel> */}
-
-    <MupPanel id="chromecastPanel" title="Cast Icon" icon={<CastConnectedIcon />}>
-      <Box display="flex" flexDirection="column" style={{ gap: '8px' }}>
-        <Skeleton variant="rect" animation="wave" height={10} />
-        <Skeleton variant="rect" animation="wave" height={20}  />
-        <Skeleton variant="rect" animation="wave" height={10}
-          width="80%" />
-        <iframe
-          title="Random Wiki article"
-          style={{
-            filter: 'opacity(0.5) grayscale(0.5)',
-            width: '100%',
-            height: '600px',
-            border: '1px dotted #CCC',
-            borderRadius: '8px'
-          }}
-          src={'https://en.wikipedia.org/wiki/Chromecast'}
-        />
-        <Skeleton variant="rect" animation="wave" height={25} />
-        <Skeleton variant="rect" animation="wave" height={15}  />
-      </Box>
-    </MupPanel>
-
-    <MupPanel id="tralalaPanel" title="Sub Demo Panel TextMock" iconInHeader={false}
-      hint="No icon big guy..." icon={<AmpStories />}>
-      <Skeleton variant="rect" width={'100%'} height={300} />
-    </MupPanel>
 
     <MuiStatusBar style={{
       // height: '32px',
@@ -153,14 +66,10 @@ function ImplementationFrame({
     }}/>
 
     <MupContent>
-      {/* <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-        <AspectRatioIcon style={{ fontSize: 40 }} color="action" />
-      </div> */}
       <iframe
         title="Random Wiki article"
         style={{
           width: '100%',
-          // filter: 'opacity(0.75) grayscale(0.5)',
           backgroundColor: '#FFF',
           height: '100%',
           border: '0px none'
@@ -168,9 +77,6 @@ function ImplementationFrame({
         src={window.location.search.split('url=')[1] || wikiUrl}
       />
     </MupContent>
-
-
-
   </MuiPanelProvider>
 }
 
--- src/MuiPanelManager/index.js
+++ src/MuiPanelManager/index.js
@@ -1,112 +1,17 @@
 import { Box } from '@material-ui/core'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import DataProvider from '../MuiPanelStore'
-import MuiSplitter from '../MuiSplitter'
-import MuiStatusBar from '../MuiStatusBar'
-import MupMenuCollapseButton from '../MupMenuCollapseButton'
 import InternalStatus from './../MuiStatusBar/InternalStatus'
-import MuiMenuButton from './MuiMenuButton'
 
-const menuWidth = '56px'
-const leftPanelWidth = '500px'
-const rightPanelWidth = '500px'
-
-const styledGrid = {
-  bothGrid: {
-    'grid-template-columns': 'auto auto 1fr auto auto',
-    'grid-template-areas':`
-      "leftMenu leftPanel main rightPanel rightMenu"
-    `
-  },
-  leftGrid: {
-    'grid-template-columns': 'auto auto 1fr',
-    'grid-template-areas':`
-      "leftMenu leftPanel main"
-    `
-  },
-  rightGrid: {
-    'grid-template-columns': '1fr auto auto',
-    'grid-template-areas':`
-      "main rightPanel rightMenu"
-    `
-  },
-}
-
-const styledMenus = theme => ({
-  bothMenus: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    position: 'relative',
-    height: '100%',
-  },
-  leftMenu: {
-    'grid-area': 'leftMenu',
-    width: menuWidth,
-    borderRight: `1px solid ${theme.palette.divider}`
-  },
-  rightMenu: {
-    width: menuWidth,
-    'grid-area': 'rightMenu',
-    borderLeft: `1px solid ${theme.palette.divider}`
-  },
-})
-
-const styledPanel = theme => ({
-  panelContainerWrapper: {
-    position: 'relative',
-    overflow: 'hidden auto',
-    display: 'contents',
-
-    '&::-webkit-scrollbar' : {
-      display: 'none'
-    }
-  },
-  panelContainer: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    // height: '100%',
-  },
-  leftPanel: {
-    gridArea: 'leftPanel',
-    borderRight: `1px solid ${theme.palette.divider}`,
-    display: 'unset'
-  },
-  rightPanel: {
-    borderLeft: `1px solid ${theme.palette.divider}`,
-    display: 'unset',
-    gridArea: 'rightPanel',
-  },
-})
-
-const useStyles = makeStyles((theme, upperbar) => ({
+const useStyles = makeStyles((theme) => ({
   wrapper: {
     height: '100%',
     width: '100%',
-
     position: 'absolute',
-
-  },
-  scrollSide: {
-    overflow: 'visible',
-    position: 'absolute',
-    left: '0px',
-    bottom: '0px',
-    top: '0px',
-    right: '0px',
-
-    '&::-webkit-scrollbar': {
-      display: 'none'
-    }
   },
   root: {
-    // height: "100%",
     flex: '1 1 auto',
-    // position: "absolute",
     width: '100%',
     overflow: 'hidden',
     display: 'grid',
@@ -115,194 +20,22 @@ const useStyles = makeStyles((theme, upperbar) => ({
     'grid-auto-flow': 'row',
     backgroundColor: theme.palette.background.default,
   },
-  ...styledGrid,
-  ...styledMenus(theme),
-  ...styledPanel(theme),
-
-  main: { 'grid-area': 'main' },
-  iconButton: {
-    fontSize: '26px',
-
-    '&:hover': {
-      color: theme.palette.primary.main,
-    }
-  },
-  buttonMenu: {
-    border: '0px none',
-    padding: theme.spacing(2, 0),
-    borderRadius: '0px',
-    minWidth: 'initial',
-  },
-  rightGroupButtonMenu: {
-    position: 'relative',
-    '&::after': {
-      content: '"G"',
-      backgroundColor: '#CCC',
-      position: 'absolute',
-      fontSize: '8px',
-      lineHeight: '12px',
-      width: '12px',
-      top: 2,
-      borderRadius: '4px',
-      left: 2,
-    },
-  },
-  leftGroupButtonMenu: {
-    position: 'relative',
-    '&::after': {
-      content: '"G"',
-      backgroundColor: '#CCC',
-      position: 'absolute',
-      fontSize: '8px',
-      lineHeight: '12px',
-      width: '10px',
-      top: 2,
-      borderRadius: '4px',
-      right: 2,
-    },
-  },
-
-  rightButtonMenu: {
-    borderRight: '4px solid transparent',
-  },
-  leftButtonMenu: {
-    borderLeft: '4px solid transparent'
-  },
-  rightActiveButtonMenu: {
-    borderRight: `4px solid ${theme.palette.primary.main}`,
-  },
-  leftActiveButtonMenu: {
-    borderLeft: `4px solid ${theme.palette.primary.main}`,
-  },
-  menuOpen: {
-    width: menuWidth,
-  },
-  menuCollapsed: {
-    width: '0px',
-    border: '0px',
-    backgroundColor: theme.palette.background.paper,
-    transition: 'background-color 350ms ease-out 100ms',
-    opacity: 1,
-    cursor: 'pointer',
-
-    '&:hover': {
-      backgroundColor: theme.palette.background.default,
-    }
-  },
-  emptySpace: {
-    flex: '1 1 auto'
-  }
 }))
 
-const availableSides = ['left', 'right']
-
-const MuiPanelManager = ({
-  children,
-  allowRightClick = false,
-  showCollapseButton = true,
-  showSplitterButton = true,
-}) => {
+const MuiPanelStatus = ({ children }) => {
   const theme = useTheme()
-  const [sides, setSides] = useState()
-  const matches = useMediaQuery('(min-width:600px)')
-  const { settings, sections, toggleSettingIsCollapsed, layout } = useContext(DataProvider)
+  const { settings } = useContext(DataProvider)
   const classes = useStyles(theme, settings.upperBar)
 
-  useEffect(() => {
-    toggleSettingIsCollapsed(!matches)
-  }, [matches])
-
-  useEffect(() => {
-    if (layout.length > 0) {
-      const foundSides = [
-        ...new Set(layout.reduce((acc, val) => {
-          acc.push(val.side)
-
-          return acc
-        }, []))]
-      setSides(foundSides.length === 1 ? foundSides[0] : 'both')
-    }
-  }, [layout])
-
-  return <Box id="MuiPanelManager"
+  return <Box
+    id="MuiPanelStatus"
     display="flex"
     flexDirection={settings.upperBar ? 'column-reverse' : 'column'}
-    className={classes.wrapper}>
-    <div
-      id="MuiPanels"
-      className={`${classes.root} ${classes[`${sides}Grid`]}`}>
-      {availableSides
-        .filter(side => layout.some(lo => lo.side === side && !lo.asContent))
-        .map(side => <div
-          key={`${side}_panels`}
-          id={`MuiPanels_${side}Side`}
-          onContextMenu={(e) => { !allowRightClick && e.preventDefault() }}
-          className={`
-          ${classes.panelContainerWrapper}
-          ${layout.some(l => l.side === side && !l.asSection && !l.asContent && l.isVisible) &&
-            (side === 'left' ? classes.leftPanel : classes.rightPanel)}`}
-        >
-          <div
-            id={`${side}-panel`} key={`${side}-panel`}
-            className={`${classes.panelContainer}`}
-            style={{
-              gridArea: `${side}Panel`,
-              overflow: 'hidden auto',
-              width: settings.isCollapsed
-                ? '0px'
-                : (layout.some(l => l.side === side && l.isVisible && !l.asSection)
-                  ? (side === 'left'
-                    ? leftPanelWidth
-                    : rightPanelWidth)
-                  : 'unset'),
-              height: layout.filter(l => l.side === side && l.isVisible).length > 1 ? '100%' : '100%'
-            }}
-          />
-        </div>)}
-
-      {availableSides
-        .filter(side => layout.some(lo => lo.side === side && !lo.asContent))
-        .map(side => <div key={`${side}_menus`}>
-          {layout.filter(lo => lo.side === side && !lo.asContent && !lo.asSection).length > 0 &&
-              <div
-                id={`${side}-menu`}
-                onDoubleClick={() => {settings.isCollapsed && toggleSettingIsCollapsed() } }
-                onContextMenu={(e) => { !allowRightClick && e.preventDefault() }}
-                className={`${classes[`${side}Menu`]} ${classes.bothMenus} ${settings.isCollapsed
-                ? classes.menuCollapsed
-                : classes.menuOpen}`}
-              >
-                {/* {showCollapseButton && <MupMenuCollapseButton {...{ side }} />} */}
-                <div className={`${classes.scrollSide}`}>
-                  {!settings.isCollapsed && <>
-                    {layout
-                      .filter(lo => lo.side === side)
-                      .filter(lo => !lo.asEmbedded && !lo.asContent && !lo.asSection)
-                      .map(lo => <MuiMenuButton
-                        extraIcons={layout.filter(l => lo.uniqueId === l.parentId).map(l => l.icon)}
-                        key={lo.uniqueId}
-                        {...{ lo, side }}
-                      />)}
-                    <div className={classes.emptySpace} onDoubleClick={() => { !settings.isCollapsed && toggleSettingIsCollapsed() }} />
-                  </>}
-                </div>
-              </div>
-          }
-        </div>)}
-
-      <div style={{ gridArea: 'main', display: 'flex', minWidth: '500px' }}>
-        {sections.filter(section => !section.parentId).map(section => <MuiSplitter {...{ showSplitterButton }} key={ section.id} section={section}
-          isRoot />)}
-      </div>
-
-      {children}
-
-    </div>
-
-    <div id="material-ui-panel-statusBar">
-      {!settings.statusBarAnnounced && <InternalStatus />}
-    </div>
+    className={classes.wrapper}
+  >
+    <div id="MuiPanels" className={`${classes.root}`}>{children}</div>
+    <div id="material-ui-panel-statusBar">{!settings.statusBarAnnounced && <InternalStatus />}</div>
   </Box>
 }
 
-export default MuiPanelManager
+export default MuiPanelStatus
