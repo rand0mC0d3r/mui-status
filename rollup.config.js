d734431c28ba292b998bada6b1e213efe793f9d9
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-14 10:50:14 +0200

    updated build chain

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import multiInput from 'rollup-plugin-multi-input'
+import external from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
+
+export default [
+  {
+    input: ['./src/**/*.js'],
+    output: [
+      {
+        dir: 'dist/',
+        format: 'es',
+        exports: 'named',
+      },
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react'],
+        babelHelpers: 'bundled',
+      }),
+      multiInput(),
+      resolve(),
+      commonjs(),
+      external(),
+      terser({ numWorkers: 2 }),
+    ],
+  },
+]
