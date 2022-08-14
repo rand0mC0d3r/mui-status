de1ff381024c6b1d06877c2075c84a33e7ba56b7
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-14 14:45:49 +0200

    added file

---
+++ main.js
@@ -0,0 +1,13 @@
+const MuiStatus = require('./lib/MuiStatus')
+const MuiStatusBar = require('./lib/MuiStatusBar')
+const MuiStatusChild = require('./lib/MuiStatusChild')
+const MuiStatusPanel = require('./lib/MuiStatusPanel')
+const MuiStore = require('./lib/MuiStore')
+
+module.exports = {
+  MuiStore: MuiStore,
+  MuiStatusPanel: MuiStatusPanel,
+  MuiStatusChild: MuiStatusChild,
+  MuiStatusBar: MuiStatusBar,
+  MuiStatus: MuiStatus,
+}
