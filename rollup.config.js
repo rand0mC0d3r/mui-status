51703737ad6442f0a04b0cbf549098005f4ffad4
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-14 18:43:28 +0200

    built next version

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,34 @@
-import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import multiInput from 'rollup-plugin-multi-input'
-import external from 'rollup-plugin-peer-deps-external'
-import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+
+const packageJson = require('./package.json')
 
 export default [
   {
-    input: ['./src/**/*.js'],
+    input: 'src/index.ts',
     output: [
       {
-        dir: 'dist/',
-        format: 'es',
-        exports: 'named',
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
       },
     ],
     plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
-        babelHelpers: 'bundled',
-      }),
-      multiInput(),
       resolve(),
       commonjs(),
-      external(),
-      terser({ numWorkers: 2 }),
+      typescript({ tsconfig: './tsconfig.json' }),
     ],
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
   },
 ]
