6662e58f252af15eb85f74a1081a9c0a44be2745
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-16 18:29:57 +0200

    built next version

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React from 'react'
 
 const SayHello = ({ name }: { name: string }): JSX.Element => (
   <div>Hey {name}, go hello to TypeScript.</div>
-);
+)
 
-export default SayHello;
+export default SayHello
