10231fe6313c7652f9d41ea1e032cc13dd2becf6
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-16 18:36:53 +0200

    built next version

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="react" />
+import MuiStatusChild from './MuiStatusChild';
 declare const SayHello: ({ name }: {
     name: string;
 }) => JSX.Element;
 export default SayHello;
+export { MuiStatusChild };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import MuiStatusChild from './MuiStatusChild'
 
 const SayHello = ({ name }: { name: string }): JSX.Element => (
   <div>Hey {name}, go hello to TypeScript.</div>
 )
 
 export default SayHello
+export { MuiStatusChild }
