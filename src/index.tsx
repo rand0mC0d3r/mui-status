22aa1402e48069e1a0c6373181629fc78f5d714b
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-28 20:50:54 +0200

    fixed build chain

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react'
+import React from 'react'
 // import MuiStatus from './MuiStatus'
 // import MuiStatusBar from './MuiStatusBar'
 // import MuiStatusChild from './MuiStatusChild'
@@ -10,9 +10,9 @@ import SayBye from './SayBye'
 //
 const SayHello = ({ name }: { name: string }): JSX.Element => {
 
-  useEffect(() => {
-    console.log('SayHello fff useEffect')
-  } , [])
+  // useEffect(() => {
+  //   console.log('SayHello fff useEffect')
+  // } , [])
 
   return (<div>Hey {name}, bla bla 33e34</div>)
 }
