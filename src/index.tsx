aeade642a9f90805143c659567c4ea7e8a3aa530
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-28 22:40:20 +0200

    works

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect } from 'react'
+// import React from 'react'
 // import MuiStatus from './MuiStatus'
 // import MuiStatusBar from './MuiStatusBar'
 // import MuiStatusChild from './MuiStatusChild'
@@ -10,11 +11,11 @@ import SayBye from './SayBye'
 //
 const SayHello = ({ name }: { name: string }): JSX.Element => {
 
-  // useEffect(() => {
-  //   console.log('SayHello fff useEffect')
-  // } , [])
+  useEffect(() => {
+    console.log('SayHello fff useEffect')
+  } , [])
 
-  return (<div>Hey {name}, bla bla 33e34</div>)
+  return (<div>Hey {name}, bla bla 33eff344d</div>)
 }
 
 export default SayHello
