ca9dedcc919711eab9feb3cc8fe919ad9c57744f
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-10-10 10:48:50 +0200

    updated UI

---
+++ src/utils/Tooltip/index.tsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import DataProvider from '../../MuiStore'
+
+export default function ({
+  tooltip = '',
+  children,
+} : {
+  tooltip?: React.ReactNode | string,
+  children?: React.ReactNode,
+}) {
+  const { tooltipComponent } = useContext(DataProvider)
+
+  return <>{tooltipComponent !== undefined ? tooltipComponent(tooltip, <span>{children}</span>) : children}</>
+}
