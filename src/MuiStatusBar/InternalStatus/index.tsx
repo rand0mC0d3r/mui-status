c837dd7fc81aa903a9d6aa531a647f518839a83d
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-10-14 21:17:23 +0200

    built next version

--- src/MuiStatusBar/InternalStatus/index.tsx
+++ src/MuiStatusBar/InternalStatus/index.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles'
 import React, { useContext } from 'react'
 import DataProvider from '../../MuiStore'
 
-const StyledStatusBar = styled('div')(({ theme }: any) => ({
+const StyledStatusBar = styled('div')<{ position?: string }>(({ theme, position }: any) => ({
   gap: '4px',
   display: 'flex',
   minHeight: '28px',
@@ -12,8 +12,8 @@ const StyledStatusBar = styled('div')(({ theme }: any) => ({
     ? theme.palette.background.default
     : theme.palette.background.paper,
   color: `${theme.palette.background.default} !important`,
-  // borderBottom: position === 'top' ? `1px solid ${theme.palette.divider}` : 'none',
-  // borderTop: position === 'top' ? 'none' : `1px solid ${theme.palette.divider}`,
+  borderBottom: position === 'top' ? `1px solid ${theme.palette.divider}` : 'none',
+  borderTop: position === 'top' ? 'none' : `1px solid ${theme.palette.divider}`,
 }))
 
 const StyledPrimaryElem = styled('div')(() => ({
@@ -55,10 +55,10 @@ export default function ({
   style?: React.CSSProperties,
   className?: React.HTMLAttributes<HTMLDivElement>['className'],
 }) {
-  const { status } = useContext(DataProvider)
+  const { status, settings } = useContext(DataProvider)
 
   return <>
-    {status.some(sItem => sItem.visible) && <StyledStatusBar {...{ id: `${domId}-wrapper`, style, className }}>
+    {status.some(sItem => sItem.visible) && <StyledStatusBar position={settings.position} {...{ id: `${domId}-wrapper`, style, className }}>
       {status.some(sItem => !sItem.secondary) && <StyledPrimaryElem {...{ id: `${domId}-primary` }} />}
       {status.some(sItem => sItem.secondary) && <StyledSecondaryElem {...{ id: `${domId}-secondary` }} />}
     </StyledStatusBar>}
--- src/MuiStatusPanel/index.tsx
+++ src/MuiStatusPanel/index.tsx
@@ -23,7 +23,7 @@ const StyledContainer = styled('div')(({ theme }) => ({
   backgroundColor: 'rgba(255,255,255,0.8)',
   backdropFilter: 'blur(5px)',
   borderRadius: '4px',
-  border: `1px solid ${theme.palette.divider}`,
+  border: `1px solid ${theme.palette.primary.dark}`,
 }))
 
 export default function ({
